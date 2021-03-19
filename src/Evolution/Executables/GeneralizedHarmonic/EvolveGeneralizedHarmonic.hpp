ab1028317b00c90fcc43978ebe237ebdbe31b119
sierra.c.thomas@gmail.com <sierra.c.thomas@gmail.com>
2021-03-18 17:07:44 -0700

    Add Weyl Electric to executable

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
@@ -99,6 +99,7 @@
 #include "PointwiseFunctions/GeneralRelativity/Ricci.hpp"
 #include "PointwiseFunctions/GeneralRelativity/SpatialMetric.hpp"
 #include "PointwiseFunctions/GeneralRelativity/Tags.hpp"
+#include "PointwiseFunctions/GeneralRelativity/WeylElectric.hpp"
 #include "Time/Actions/AdvanceTime.hpp"
 #include "Time/Actions/ChangeSlabSize.hpp"
 #include "Time/Actions/ChangeStepSize.hpp"
@@ -183,6 +184,7 @@ struct EvolutionMetavars {
   using observe_fields = tmpl::append<
       tmpl::push_back<
           analytic_solution_fields,
+          gr::Tags::WeylElectricScalar<DataVector>,
           ::Tags::PointwiseL2Norm<
               GeneralizedHarmonic::Tags::GaugeConstraint<volume_dim, frame>>,
           ::Tags::PointwiseL2Norm<GeneralizedHarmonic::Tags::
