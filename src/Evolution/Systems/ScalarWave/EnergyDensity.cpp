282fbb53aa1b5c0a3690abcd1c7bdddc20c98d80
marlo <marlo@wheeler.caltech.edu>
2021-06-22 15:38:05 -0700

    fixup energy density function and cmake targets

--- src/Evolution/Systems/ScalarWave/EnergyDensity.cpp
+++ src/Evolution/Systems/ScalarWave/EnergyDensity.cpp
@@ -5,6 +5,7 @@
 
 #include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
 #include "DataStructures/Tensor/Tensor.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
 #include "Utilities/Gsl.hpp"
 
 namespace ScalarWave {
@@ -17,3 +18,16 @@ void energy_density(
       0.5 * (get(pi) * get(pi) + get(magnitude(phi)) * get(magnitude(phi)));
 }
 }  // namespace ScalarWave
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                           \
+  template void ScalarWave::energy_density(            \
+      const gsl::not_null<Scalar<DataVector>*> result, \
+      const Scalar<DataVector>& pi,                    \
+      const tnsr::i<DataVector, DIM(data), Frame::Inertial>& phi) noexcept;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3))
+
+#undef INSTANTIATE
+#undef DIM
