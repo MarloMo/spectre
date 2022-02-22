7ce21375fd6e72ddb6f2552f5072307a2a63ea75
MarloMo <moralesmarlo486@gmail.com>
2022-02-22 10:49:02 -0800

    Add ZeroCrossingPredictor test function

---
+++ tests/Unit/NumericalAlgorithms/Fitting/Test_ZeroCrossingPredictor.cpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include "DataStructures/DataVector.hpp"
+#include "Utilities/Gsl.hpp"
+
+SPECTRE_TEST_CASE("Unit.FiniteDifference.LinearFit",
+                  "[Unit][NumericalAlgorithms]") {
+
+const std::array<double, 2> x{{0.0, 0.0}};
+const std::array<double, 2> y{{0.0, 1.0}};
+
+}
